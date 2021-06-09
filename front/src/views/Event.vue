7d151c223a9dc9ae13d28e481fdecf53a27dfdc7
aymmenkhs <aymenkhouas@gmail.com>
2021-06-09 14:41:24 +0100

    Event first version

--- front/src/views/Event.vue
+++ front/src/views/Event.vue
@@ -1,6 +1,44 @@
 <template>
     <div class="hero">
+        <div class="space"></div>
+        <b-container fluid class="page">
+            <b-row align-v="center" align-h="center">
+                <b-col class="leftside" md="6" sm="12">
+                    <h2>Tout sur L'Install Party</h2>
+                    <img class="large_white_line"  src="../assets/large_white_line.png"/>
+                    <p>
+                        L'Install Party c'est des conférences des workshops mais plus important c'est un evenement sur l'esprit du libre et du partage !
+                    </p>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate, pariatur, quos corrupti iusto nulla corporis dolore accusamus similique eligendi facilis est quo non inventore quibusdam culpa dolor incidunt ipsa debitis!
+                    </p>
+                </b-col>
+                <b-col class="rightside" md="6" sm="12">
+                    <div class="">
+                        <b-row align-h="center">
+                            <b-col md="3">
+                                <img class="clubLogo" src="../assets/omc_logo_white.png"/>
+                            </b-col>
+                            <b-col md="3">
+                                <img class="clubLogo" src="../assets/omc_logo_white.png"/>
+                            </b-col>
+                            <b-col md="3">
+                                <img class="clubLogo" src="../assets/omc_logo_white.png"/>
+                            </b-col>
+                        </b-row>
+                        <b-row align-h="center" align-v="center">
+                            <b-col md="7">
+                                <div>
+                                    <b-img center  src="/img/logoIP10_edition.88a91c9e.png" fluid alt="Responsive image" class="IPlogo"></b-img>
+                                </div>
+                            </b-col>
+                        </b-row>
 
+                    </div>
+                </b-col>
+            </b-row>
+        </b-container>
     </div>
 </template>
 
@@ -22,8 +60,83 @@ export default {
     background-repeat: no-repeat;
     background-size: cover;
     position: relative;
-
-    display: flex;
-    flex-direction: column;
 }
+
+.space{
+    height: 15%;
+}
+
+.page{
+    padding-left: 3%;
+    padding-right: 3%;
+}
+
+.leftside{
+    padding-right: 10%;
+}
+
+.leftside h2{
+    font-size: 3em;
+    color: white;
+}
+
+.leftside img{
+    width:90%;
+}
+
+.leftside p{
+    color: white;
+    font-size: 20px;
+}
+
+.rightside .clubLogo{
+    width:100%;
+}
+
+.rightside .IPlogo{
+    width:100%;
+    margin-top: 5%;
+}
+
+
+@media(max-width: 1300px){
+    .leftside h2{
+        font-size: 2em;
+    }
+
+    .leftside p{
+        font-size: 16px;
+    }
+}
+
+@media(max-width: 900px){
+    .leftside p{
+        font-size: 13px;
+    }
+}
+
+@media(max-width: 770px){
+
+    .space{
+        height: 5%;
+    }
+
+    .leftside h2{
+        font-size: 2em;
+    }
+
+    .leftside p{
+        font-size: 16px;
+    }
+
+    .rightside .clubLogo{
+        display: none;
+    }
+
+    .rightside .IPlogo{
+        width:50%;
+        margin-top: 10%;
+    }
+}
+
 </style>
