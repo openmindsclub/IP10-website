6e23d0ad95f3cb6f68c7ca8c0172790ee3c80fcc
aymmenkhs <aymenkhouas@gmail.com>
2021-06-09 02:39:35 +0100

    init views

--- front/src/App.vue
+++ front/src/App.vue
@@ -2,17 +2,33 @@
   <div>
     <NavBar/>
     <router-view/>
+    <Event/>
+    <Conferances/>
+    <Workshops/>
+    <Activities/>
+    <Stands/>
   </div>
 </template>
 
 <script>
 import NavBar from './components/NavBar'
 import Acceuil from './views/Acceuil'
+import Event from './views/Event'
+import Conferances from './views/Conferances'
+import Workshops from './views/Workshops'
+import Activities from './views/Activities'
+import Stands from './views/Stands'
+
 export default {
   name: 'App',
   components: {
     NavBar,
-    Acceuil
+    Acceuil,
+    Event,
+    Conferances,
+    Workshops,
+    Activities,
+    Stands
   }
 }
 </script>
---
+++ front/src/views/Workshops.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="hero">
+
+    </div>
+</template>
+
+<script>
+export default {
+  name: "Workshops",
+};
+</script>
+
+
+<style scoped >
+.hero{
+    background-image: url("../assets/backgrounds/Event_background_3.jpg");
+
+    height: 100vh;
+    width: 100%;
+
+    background-position: center;
+    background-repeat: no-repeat;
+    background-size: cover;
+    position: relative;
+
+    display: flex;
+    flex-direction: column;
+}
+</style>
