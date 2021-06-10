d918937697de718dbcda7e04f0f45991a9c0e8db
aymmenkhs <aymenkhouas@gmail.com>
2021-06-10 14:00:29 +0100

    navbar drop downs

--- front/src/components/NavBar.vue
+++ front/src/components/NavBar.vue
@@ -7,7 +7,13 @@
         <div class="menu">
         	<ul v-bind:class="{showing: sidebar}">
 				<li v-for="item in items" :key="item.title">
-					<a :href="item.path">{{ item.title }}</a>
+					<a v-if= "!item.composed" :href="item.path">{{ item.title }}</a>
+					<div v-else class="dropdown">
+  						<p class="dropbtn">{{item.title}}</p>
+  						<div class="dropdown-content">
+							<a  v-for="subItem in item.components" :key="subItem.title" :href="subItem.path">{{ subItem.title }}</a>
+  						</div>
+					</div>
 				</li>
             </ul>
         </div>
@@ -21,10 +27,18 @@ export default {
 	data: () => ({
 		sidebar: false,
 		items: [
-			{ title: "Event", path: "/#/Event"},
-			{ title: "Activités", path: "/#/Activités"},
-			{ title: "Inscriptions", path: "/#/Inscriptions"},
-			{ title: "A propos", path: "/#/apropos"},
+			{ title: "Acceuil", path: "/#/welcome", composed: false},
+			{ title: "Event", path: "/#/event", composed: false},
+			{ title: "Activities", composed: true, components:[
+				{ title: "Conferances", path: "/#/conferences"},
+				{ title: "Workshops", path: "/#/workshops"},
+				{ title: "Other Activities", path: "/#/activities"},
+				{ title: "Stands", path: "/#/stands"}
+			]},
+			{ title: "Inscriptions", path: "/#/registration", composed: false},
+			{ title: "A propos", composed: true, components:[
+				{ title: "A propos", path: "/#/about"},
+			]},
 		]
 	}),
 	methods: {
@@ -73,8 +87,49 @@ nav ul li a {
     font-size: 18px;
 }
 
+nav ul li p {
+    font-family: 'Bungee', cursive;
+    text-decoration: none !important;
+    color: #4B535C;
+    font-size: 18px;
+}
+
 nav ul li a:hover{
     color: #E1CF4B;
+}
+
+/* Dropdown Button */
+.dropbtn {
+  border: none;
+}
+
+/* The container <div> - needed to position the dropdown content */
+.dropdown {
+  display: inline-block;
+}
+
+/* Dropdown Content (Hidden by Default) */
+.dropdown-content {
+  display: none;
+  color: #F2F5FE;
+  position: fixed;
+  z-index: 99999;
+}
+
+/* Links inside the dropdown */
+.dropdown-content a {
+  text-decoration: none;
+  display: block;
+}
+
+/* Show the dropdown menu on hover */
+.dropdown:hover .dropdown-content {
+	display: block;
+}
+
+
+nav a.router-link-exact-active {
+  text-decoration: underline;
 }
 
 .menu-icon {
