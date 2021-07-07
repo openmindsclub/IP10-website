0070b0c2167b183ee93ace1ade1246e6fa3e013d
aymmenkhs <aymenkhouas@gmail.com>
2021-07-07 23:16:35 +0100

    sessions modification

--- front/src/components/Paragraph.vue
+++ front/src/components/Paragraph.vue
@@ -3,15 +3,54 @@
         <h4 :class="text_color">{{ title }}</h4>
         <img class="large-line" :class="show_white_line" src="../assets/large_white_line.png"/>
         <img class="large-line" :class="show_gray_line" src="../assets/large_gray_line.png"/>
-        <h6 v-if="time" class="time" :class="text_color">{{begin}} - {{end}}</h6>
+        <div v-if="time">
+            <h6 class="time" :class="text_color">{{type}} : {{begin}} - {{end}}</h6>
+            <div v-if="images.length > 0" class="images">
+                <div v-for="speaker in images" :key="speaker.index" >
+                    <b-img class="image" rounded="circle" :src="speaker.image" :alt="speaker.name" :title="speaker.name" @click="showModal(speaker.index)"></b-img>
+                </div>
+            </div>
+        </div>
         <p :class="text_color">{{ content }}</p>
+        <b-modal v-model="showModalBool" centered :title="modalHeader">
+            <p>{{modalContent}}</p>
+            <div class="modal-contener">
+                <b-img class="modal-image" rounded="circle" :src="modalImage" :alt="modalHeader" :title="modalHeader"></b-img>
+            </div>
+        </b-modal>
     </div>
 </template>
 
 <script>
 export default {
   name: "Paragraph",
-  props: ['title', 'content', 'color', 'time', 'begin', 'end'],
+  props: ['title', 'content', 'color', 'time', 'begin', 'end', 'type', 'images'],
+  data: () => ({
+      showModalBool: false,
+      modalContent: "",
+      modalImage: "",
+      modalHeader: "",
+  }),
+  methods: {
+            showModal(id) {
+                console.log(id)
+                console.log(this.images)
+                for (let image of this.images){
+                    if (image.index == id){
+                        console.log(image.index)
+                        console.log(id)
+                        this.modalContent = image.description
+                        this.modalImage = image.image
+                        this.modalHeader = image.name
+                        console.log(this.modalContent)
+                    }
+                }
+                this.showModalBool = true
+            },
+            hideModal() {
+                this.showModalBool = false
+            },
+  },
   computed: {
       text_color(){
           if (this.color === 'gray'){
@@ -81,4 +120,50 @@ p{
 .dont-display-line {
     display: none;
 }
+
+.images{
+    display: flex
+}
+
+.image{
+    width: 30px;
+    height: 30px;
+    margin-right: 5px;
+}
+
+.modal-image{
+    width: 300px;
+    height: 300px;
+}
+
+.modal-contener{
+    display: flex;
+    justify-content: center;
+}
+
+
+@media(max-width: 992px){
+    .modal-image{
+        width: 250px;
+        height: 250px;
+    }
+}
+
+@media(max-width: 800px){
+
+    .modal-image{
+        width: 150px;
+        height: 150px;
+    }
+}
+
+@media(max-width: 600px){
+
+    .modal-image{
+        width: 100px;
+        height: 100px;
+    }
+}
+
+
 </style>
--- front/src/components/Sessions.vue
+++ front/src/components/Sessions.vue
@@ -5,7 +5,7 @@
             <b-container fluid>
                 <b-row align-v="center" align-h="center">
                     <b-col xl="8" lg="10" md="12">
-                        <Paragraph :title="conf1.title" :content="conf1.content" time=true :begin="conf1.timeBegin" :end="conf1.timeEnd"/>
+                        <Paragraph :title="conf1.title" :content="conf1.content" :type="conf1.type" time=true :begin="conf1.timeBegin" :end="conf1.timeEnd" :images="conf1.speakers"/>
                     </b-col>
                 </b-row>
                 <b-row align-v="center" align-h="center">
@@ -14,21 +14,21 @@
                 <b-row  align-h="center">
                     <b-col  xl="5" lg="6" md="12">
                         <transition appear name="come-from-right">
-                            <Paragraph :title="conf2.title" :content="conf2.content" time=true :begin="conf2.timeBegin" :end="conf2.timeEnd"/>
+                            <Paragraph :title="conf2.title" :content="conf2.content" :type="conf2.type" time=true :begin="conf2.timeBegin" :end="conf2.timeEnd" :images="conf2.speakers"/>
                         </transition>
                         <div class="space2"></div>
                         <transition appear name="come-from-right">
-                            <Paragraph :title="conf3.title" :content="conf3.content" time=true :begin="conf3.timeBegin" :end="conf3.timeEnd"/>
+                            <Paragraph :title="conf3.title" :content="conf3.content" :type="conf3.type" time=true :begin="conf3.timeBegin" :end="conf3.timeEnd" :images="conf3.speakers"/>
                         </transition>
                     </b-col>
                     <b-col  xl="5" lg="6" md="12">
                         <div class="space"></div>
                         <transition appear name="come-from-left">
-                            <Paragraph :title="panel1.title" :content="panel1.content" time=true :begin="panel1.timeBegin" :end="panel1.timeEnd"/>
+                            <Paragraph :title="panel1.title" :content="panel1.content" :type="panel1.type" time=true :begin="panel1.timeBegin" :end="panel1.timeEnd" :images="panel1.speakers"/>
                         </transition>
                         <div class="space"></div>
                         <transition appear name="come-from-left">
-                            <Paragraph :title="panel2.title" :content="panel2.content" time=true :begin="panel2.timeBegin" :end="panel2.timeEnd"/>
+                            <Paragraph :title="panel2.title" :content="panel2.content" :type="panel2.type" time=true :begin="panel2.timeBegin" :end="panel2.timeEnd" :images="panel2.speakers"/>
                         </transition>
                     </b-col>
                 </b-row>
@@ -37,7 +37,7 @@
         <div class="mobile">
             <carousel :autoplay="true" :autoplayHoverPause="true" :perPage="1" :loop="true">
                 <slide v-for="conf in conferenceAndPanels" :key="conf.tittle" >
-                    <Paragraph :title="conf.title" :content="conf.content" time=true :begin="conf.timeBegin" :end="conf.timeEnd"/>
+                    <Paragraph :title="conf.title" :content="conf.content" :type="conf.type" time=true :begin="conf.timeBegin" :end="conf.timeEnd" :images="conf.speakers"/>
                 </slide>
             </carousel>
         </div>
@@ -58,34 +58,90 @@ export default {
       slide: 0,
       sliding: null,
       conf1 : {
-          title: "Conférence: La difference entre le libre et l'open source",
+          title: "La difference entre le libre et l'open source",
           content: "Le libre et l’open source, des concepts frères, mais diffèrent. Pourtant, beaucoup de gens ne font pas la différence entre eux. Dans cette conférence, nous allons donc introduire l’open source, le libre et la différence entre les deux.",
           timeBegin: "9h",
           timeEnd: "9h45",
+          type: "Conférence",
+          speakers: []
       },
       conf2 : {
-          title: "Conférence: L’open source dans le développement scientifique et les nouvelles technologies",
+          title: "L’open source dans le développement scientifique et les nouvelles technologies",
           content: "L’open source a beaucoup contribué au développement scientifique et nouvelles  technologique durant ces dernières années,  vous découvrirez donc durant cette conférence comment l’open source impacte ces deniers et bien sure l’importance de l’open source dans le progrès humain.",
           timeBegin: "10h",
           timeEnd: "10h45",
+          type: "Conférence",
+          speakers: [
+              {
+                  index: 0,
+                  name: "Aymen Khouas",
+                  smallDescription: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+                  description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+                  image: "https://picsum.photos/200/300"
+              },
+          ]
       },
       panel1 : {
-          title: "Panel: L'impact de l’open source dans le développement des nouvelles technologies en Algérie et dans le monde",
+          title: "L'impact de l’open source dans le développement des nouvelles technologies en Algérie et dans le monde",
           content: "L’open source a le potentiel de révolutionner le développement de nouvelles technologies en Algérie, il est donc toute a fait naturel pour nous de nous intéresser à cette question. Rejoignez-nous pour ce panel pour en apprendre plus, mais aussi avoir la possibilité de données votre avis.",
           timeBegin: "11h",
           timeEnd: "11h45",
+          type: "Panel",
+          speakers: [
+              {
+                  index: 0,
+                  name: "Aymen Khouas",
+                  smallDescription: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+                  description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+                  image: "https://picsum.photos/200/300"
+              },
+              {
+                  index: 1,
+                  name: "Dovahkiin",
+                  smallDescription: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+                  description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+                  image: "https://picsum.photos/200/300"
+              }
+          ]
       },
       conf3 : {
-          title: "Conférence: Géant de l’industrie qui se tourne vers l’open source",
+          title: "Géant de l’industrie qui se tourne vers l’open source",
           content: "Google, Facebook, Microsoft  ou encore Amazon, toutes ces entreprises ont une chose en commun, ils se tournent de plus en plus vers des licences open source pour leurs produits. Découvrez avec nous ce que ces géants de l'industrie on a gagné en se tournant vers l’open source.",
           timeBegin: "14h30",
           timeEnd: "15h15",
+          type: "Conférence",
+          speakers: [
+              {
+                  index: 2,
+                  name: "Aymen Khouas",
+                  smallDescription: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+                  description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+                  image: "https://picsum.photos/200/300"
+              },
+          ]
       },
       panel2 : {
-          title: "Panel: Comment l’open source peut améliorer la croissance des entreprises en Algérie",
+          title: "Comment l’open source peut améliorer la croissance des entreprises en Algérie",
           content: "De part l’importance économique qu’ont pris les entreprissent se basant sur l’open source ces dernières années, il est intéressant de se poser la question de comment l’open source peut améliorer la croissance des entreprises en Algérie ?",
           timeBegin: "15h30",
           timeEnd: "16h15",
+          type: "Panel",
+          speakers: [
+              {
+                  index: 2,
+                  name: "Aymen Khouas",
+                  smallDescription: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+                  description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+                  image: "https://picsum.photos/200/300"
+              },
+              {
+                  index: 3,
+                  name: "Dovahkiin",
+                  smallDescription: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+                  description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+                  image: "https://picsum.photos/200/300"
+              }
+          ]
       },
   }),
   methods: {
